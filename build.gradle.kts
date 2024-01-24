ac5f50c08fd560e060004848c5db2b330a6a4a01
Barney <barney@gmail.com>
2024-01-24 18:44:11 +0800

    chore: 添加maven发布插件

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.1.2" apply false
 }
+
+
--- netdoctor/build.gradle.kts
+++ netdoctor/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    //发布到远程仓库需要用到此插件
+    id("com.vanniktech.maven.publish") version "0.23.0"
 }
 
 android {
@@ -35,4 +39,15 @@ android {
 dependencies {
     implementation("dnsjava:dnsjava:3.5.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+}
+
+publishing {
+    repositories {
+        maven {
+            // $rootDir 表示项目的根目录
+            // 这里配置发布到的本地目录
+            name = "local"
+            setUrl("$rootDir/repo")
+        }
+    }
 }
